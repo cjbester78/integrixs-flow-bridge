00efe98e60a2b2ba0f389bc26087fcdafc3039a0
CJ Bester <cjbester@integixsdev1.local>
2025-09-06 23:40:17 +0200

    Fix: Remove semicolons after React.forwardRef< in input-otp.tsx

--- frontend/src/components/ui/input-otp.tsx
+++ frontend/src/components/ui/input-otp.tsx
@@ -4,7 +4,7 @@ import { Dot } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
-const InputOTP = React.forwardRef<;
+const InputOTP = React.forwardRef<
  React.ElementRef<typeof OTPInput>,
  React.ComponentPropsWithoutRef<typeof OTPInput>
 >(({ className, containerClassName, ...props }, ref) => (
@@ -20,7 +20,7 @@ const InputOTP = React.forwardRef<;
 ))
 InputOTP.displayName = "InputOTP"
 
-const InputOTPGroup = React.forwardRef<;
+const InputOTPGroup = React.forwardRef<
  React.ElementRef<"div">,
  React.ComponentPropsWithoutRef<"div">
 >(({ className, ...props }, ref) => (
@@ -28,7 +28,7 @@ const InputOTPGroup = React.forwardRef<;
 ))
 InputOTPGroup.displayName = "InputOTPGroup"
 
-const InputOTPSlot = React.forwardRef<;
+const InputOTPSlot = React.forwardRef<
  React.ElementRef<"div">,
  React.ComponentPropsWithoutRef<"div"> & { index: number }
 >(({ index, className, ...props }, ref) => {
@@ -56,7 +56,7 @@ const InputOTPSlot = React.forwardRef<;
 })
 InputOTPSlot.displayName = "InputOTPSlot"
 
-const InputOTPSeparator = React.forwardRef<;
+const InputOTPSeparator = React.forwardRef<
  React.ElementRef<"div">,
  React.ComponentPropsWithoutRef<"div">
 >(({ ...props }, ref) => (
